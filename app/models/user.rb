c62f9f006be896472ec36c6afd007a24d3a11c52
Iori Matsuhara <matsuhar@gmail.com>
2015-01-01 23:28:43 -0500

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User
+
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  devise :database_authenticatable, :recoverable, :rememberable, :registerable
+
+  field :email, type: String, default: ""
+  field :encrypted_password,     type: String, default: ""
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+  field :remember_created_at,    type: Time
 end
