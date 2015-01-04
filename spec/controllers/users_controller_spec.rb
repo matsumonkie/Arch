9b897422c5b643be0970f23b81e6045dcb67570c
Iori Matsuhara <matsuhar@gmail.com>
2015-01-04 00:04:10 -0500

    spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,18 +7,4 @@ RSpec.describe UsersController do
       expect(response).to render_template('index')
     end
   end
-
-  describe 'GET #new' do
-    it 'renders the new template' do
-      get :new
-      expect(response).to render_template('new')
-    end
-  end
-
-  describe 'POST /users' do
-    it 'should create and redirect to #show' do
-      #post :create, user: { email: 'email@email.com', password: 'password' }
-      #expect(response).to render_template('show')
-    end
-  end
 end
