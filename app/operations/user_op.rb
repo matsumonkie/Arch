2a4fed3f4c912bf4b502e6b7bdbe76a38521a8be
Iori Matsuhara <matsuhar@gmail.com>
2015-01-04 11:19:33 -0500

    ++

--- app/operations/user_op.rb
+++ app/operations/user_op.rb
@@ -2,11 +2,11 @@ class UserOp < Operation
 
   def update(params)
     User.find(params[:id]).tap do |user|
-      user.update(user_params(params))
+      user.update(whitelist(params))
     end
   end
 
-  def user_params(params)
+  def whitelist(params)
     params.require(:user).permit(:firstname, :lastname)
   end
 end
