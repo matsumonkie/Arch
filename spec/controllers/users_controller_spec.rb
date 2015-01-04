c4d795ed316e9c638bb79bcc32fa58a94acdf17a
Iori Matsuhara <matsuhar@gmail.com>
2015-01-04 00:29:00 -0500

    spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,18 @@
 require "rails_helper"
 
 RSpec.describe UsersController do
+
   describe 'GET #index' do
-    it 'renders the index template' do
+
+    it 'index' do
       get :index
       expect(response).to render_template('index')
     end
+=begin
+    it 'show' do
+      get :show
+      expect(response).to be_success
+    end
+=end
   end
 end
