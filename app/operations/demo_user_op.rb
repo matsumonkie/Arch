648376c677df84d1ba2902168bccc6bf52aa3fa4
Iori Matsuhara <matsuhar@gmail.com>
2015-01-04 22:09:58 -0500

    master spec

--- app/operations/demo_user_op.rb
+++ app/operations/demo_user_op.rb
@@ -3,8 +3,8 @@ class DemoUserOp < Operation
   def update(params)
     DemoUser.find(params[:id]).tap do |user|
       type = params[:type]
-      new_user = User.find_by(email: "#{type}@#{type}.#{type}")
-      user.update(user: new_user)
+      wrapped_user = User.find_by(email: "#{type}@#{type}.#{type}")
+      user.update(user: wrapped_user)
     end
   end
 end
--- spec/models/demo_user_spec.rb
+++ spec/models/demo_user_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 RSpec.describe DemoUser, :type => :model do
 
-  let(:admin) { build(:admin) }
-  let(:regular) { build(:regular) }
-  let(:demo_user) { build(:demo_user, { user: admin }) }
+  let(:demo_user) { create(:demo_user, { user: admin }) }
+  let(:admin) { create(:admin) }
+  let(:regular) { create(:regular) }
 
   it "wraps a user" do
     expect(demo_user.demo_user?()).to be true
     expect(demo_user.type).to eq("admin")
   end
 
-  pending "change its type"
+  it "change its type" do
+    expect(demo_user.type).to eq("admin")
+    new_demo_user = DemoUserOp.update({ id: demo_user.id, type: regular.type })
+    expect(new_demo_user.type).to eq("regular")
+  end
 end
