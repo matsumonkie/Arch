20a316b846a3c821b2569f9696d75f0c57ea84fe
Iori Matsuhara <matsuhar@gmail.com>
2015-01-04 19:00:50 -0500

    demo_user config

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
+# this is causing a bug with devise/mongoid/warden
 Rails.application.config.action_dispatch.cookies_serializer = :json
