731fb311fb0159ded21f71d9d80d837f4f853f75
Iori Matsuhara <matsuhar@gmail.com>
2015-01-07 16:34:10 -0500

    backend spec

---
+++ spec/operations/operation_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe Operation do
+
+  def nb_methods(klass)
+    (klass.public_methods - Object.public_methods).size
+  end
+
+  it 'respond_to anything' do
+    expect(UserOp.respond_to?(:index)).to be true
+  end
+
+  it 'dynamically create method' do
+    expect(nb_methods(SpecOp)).to eq(nb_methods(Operation))
+    SpecOp.index({}) # create a method
+    expect(nb_methods(SpecOp)).to eq(nb_methods(Operation) + 1)
+  end
+
+  it 'errors with undefined method' do
+    expect { UserOp.undefined_method({}) }.to raise_error(NoMethodError)
+  end
+end
