d014dd72e78d6c95b1c62b41fb539ceb4cce5155
Iori Matsuhara <matsuhar@gmail.com>
2015-01-07 20:13:35 -0500

    backend spec

---
+++ spec/lib/role_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Role, :type => :model do
+
+  let(:user) { build(:regular) }
+
+  module SpecRole
+    def do_something
+      1
+    end
+  end
+
+  it "play role" do
+    user.play(SpecRole) do |user|
+      expect(user.respond_to?(:do_something)).to be true
+      expect(user.do_something()).to eq(1)
+    end
+    expect(user.respond_to?(:do_something)).to_not be true
+  end
+end
