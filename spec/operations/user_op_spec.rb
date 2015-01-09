52307b83f0b045b800d1b4b0f366a2ea81658a58
Iori Matsuhara <matsuhar@gmail.com>
2015-01-08 21:32:18 -0500

    user op spec

--- spec/operations/user_op_spec.rb
+++ spec/operations/user_op_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe UserOp do
     users = UserOp.index({})
     expect(User.all.size).to eq(0)
   end
+
+  it 'show' do
+    user_to_show = UserOp.show({ id: user.id })
+    expect(user_to_show.type).to eq(user.type)
+  end
+
+  it 'edit' do
+    user_to_edit = UserOp.edit({ id: user.id })
+    expect(user_to_edit.type).to eq(user.type)
+  end
+
+  it 'update' do
+    params = ActionController::Parameters.new({
+      id: user.id,
+      user: {
+        firstname: "coucou",
+      }
+    })
+    updated_user = UserOp.update(params)
+    expect(updated_user.firstname).to eq(params[:user][:firstname])
+  end
 end
