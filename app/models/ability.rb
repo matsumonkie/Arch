749d33284b8ec19cb6c9dec4086402c892cb676e
Iori Matsuhara <matsuhar@gmail.com>
2015-01-13 14:16:30 -0500

    fixup! signin model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,16 +7,52 @@ class Ability
   end
 
   def set_alias
-    alias_action :create, :read, :update, :destroy, :to => :crud
+    alias_action :create, :show, :index, :edit, :update, :destroy, :to => :crud
   end
 
   def set_abilities(type)
-    abilities = ABILITIES.call()
-    abilities.fetch(type).each do |model, abilities|
-      abilities = Array(abilities).map(&:to_sym)
-      model = model.to_sym
-
-      can(abilities, model)
+    case type
+    when :admin   then admin()
+    when :regular then regular()
+    when :visitor then visitor()
     end
+  end
+
+  def common_abilities
+    can :crud, :welcome
+    can :create, :sessions
+    can :show, :translations
+    can :crud, :templates
+    can [:crud, :current], :users
+  end
+
+  def signed_user_abilities
+  end
+
+  def admin_abilities
+    can :manage, :all
+  end
+
+  def regular_abilities
+  end
+
+  def visitor_abilities
+  end
+
+  def admin
+    common_abilities()
+    signed_user_abilities()
+    admin_abilities()
+  end
+
+  def regular
+    common_abilities()
+    signed_user_abilities()
+    regular_abilities()
+  end
+
+  def visitor
+    common_abilities()
+    visitor_abilities()
   end
 end
