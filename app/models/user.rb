0d76b398b8a2646bf913e6cbab4b061c2349ccae
Iori Matsuhara <matsuhar@gmail.com>
2015-01-13 10:05:51 -0500

    signin model

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,13 @@ class User
   def visitor?; false; end
   def demo_user?; false; end
   def regular?; false; end
+
+  def to_json
+    {
+      email: email,
+      firstname: firstname,
+      lastname: lastname,
+      type: type,
+    }
+  end
 end
