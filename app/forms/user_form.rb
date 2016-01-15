434d1d83eb62e5b36509465becc0fa0ce2b6d453
Iori Matsuhara <matsuhar@gmail.com>
2016-01-15 18:59:32 +0100

    ++

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -3,19 +3,9 @@ class UserForm < Form
   MODEL = :user
   WHITELIST = :firstname
 
-  #  constraint: ->() { firstname.present? }
   coerce :firstname
-  coerce :b, :c
 
   def firstname
-    Error(:firstname, :not_valid) if true
-  end
-
-  def b
-    puts "in b"
-  end
-
-  def c
-    puts "in c"
+    Error(:firstname, :not_valid) if false
   end
 end
